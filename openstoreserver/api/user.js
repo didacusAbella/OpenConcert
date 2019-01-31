2156e3379cb980acb955a8b90767b12ef118a63a
Angelo Passaro <93Ryuk@gmail.com>
2019-01-31 14:08:42 +0100

    Added packages

--- openstoreserver/api/user.js
+++ openstoreserver/api/user.js
@@ -1,8 +1,37 @@
 const express = require("express");
 const userRouter = express.Router();
+const driver = require("../db/driver");
+
+var session = driver.session();
+
 
 userRouter.get("/", function(req, res){
+    session
+    .run('MATCH (n:User) RETURN n.name AS name, n.lastName AS lastname, n.email AS email')
+    .then(function (result) {
+        response = []
+      result.records.forEach(element => {
+          response.push({"Name": element.get("name"), "Last Name": element.get("lastname"), "Email": element.get("email")})
+      });
+      session.close();
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+});
 
+userRouter.get("/login", function(req, res){
+    session
+  .run('MATCH (u:User {email : {emailParam}, password : {passwordParam} }) RETURN u', {nameParam: req.body.email, passwordName: req.body.password })
+  .then(function (result) {
+    result.records.forEach(function (record) {
+      console.log(record.get('name'));
+    });
+    session.close();
+  })
+  .catch(function (error) {
+    console.log(error);
+  });
 });
 
 
