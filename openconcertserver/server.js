f2e6bdb8a255cbcb1de02981de806e44efb695d9
Angelo Passaro <93Ryuk@gmail.com>
2019-02-01 17:41:50 +0100

    Add token

--- openconcertserver/server.js
+++ openconcertserver/server.js
@@ -1,7 +1,8 @@
 const express = require("express");
-const cookieSession = require('cookie-session')
 const bodyParser = require('body-parser');
 const serverConf = require("./config/serverconf");
+const AuthController = require('./auth/authController');
+const VerifyToken = require('./auth/verifyToken');
 const app = express();
 
 // API
@@ -15,25 +16,26 @@ const bandRoutes = require("./api/band");
 /**
  * For accept angular client
  */
-app.use(function(req, res, next){
+app.use(function (req, res, next) {
   res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
   next();
 });
 
 
-
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
+
 
 /**
  * Mount router functions
  */
-app.use("/users", userRouter);
-app.use("/genres", genreRouter);
-app.use("/locales", localeRouter);
-app.use("/bands", bandRoutes);
+app.use("/auth", AuthController);
+app.use("/users", VerifyToken, userRouter);
+app.use("/genres", VerifyToken, genreRouter);
+app.use("/locales", VerifyToken, localeRouter);
+app.use("/bands", VerifyToken, bandRoutes);
 
-app.listen(serverConf.port, serverConf.hostname, function(req, res){
+app.listen(serverConf.port, serverConf.hostname, function (req, res) {
   console.log(`Server started at 3000`);
 })
